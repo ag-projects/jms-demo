d9a29bfcf1a3939cc963ed66f1e6fc49871c311c
Armen G <gharibi.armen@yahoo.com>
2019-08-22 16:27:00 -0700

    Added a task executor bean

--- src/main/java/com/agharibi/jmsdemo/JmsDemoApplication.java
+++ src/main/java/com/agharibi/jmsdemo/JmsDemoApplication.java
@@ -1,12 +1,25 @@
 package com.agharibi.jmsdemo;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JmsDemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+
+        // Setting up simple activeMQ
+        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+                .setPersistenceEnabled(Boolean.FALSE)
+                .setJournalDirectory("target/data/journal")
+                .addAcceptorConfiguration("invm", "vm://0"));
+
+        server.start();
+
         SpringApplication.run(JmsDemoApplication.class, args);
     }
 
---
+++ src/main/java/com/agharibi/jmsdemo/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package com.agharibi.jmsdemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -2166350361023980722L;
+
+    private UUID id;
+    private String message;
+}
