71c9067dd62302cad3ec8722738626faaf3a0393
Armen G <gharibi.armen@yahoo.com>
2019-08-22 17:10:49 -0700

    Added send jms message logic

---
+++ src/main/java/com/agharibi/jmsdemo/sender/HelloSender.java
@@ -0,0 +1,31 @@
+package com.agharibi.jmsdemo.sender;
+
+
+import com.agharibi.jmsdemo.config.JmsConfig;
+import com.agharibi.jmsdemo.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class HelloSender {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage() {
+        System.err.println("I am sending a message");
+        HelloWorldMessage message = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("Hello world")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
+        System.err.println("Message sent!");
+    }
+}
